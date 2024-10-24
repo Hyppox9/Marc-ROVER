839d6db63c8dfa81a77f37126ab8dba5cf71c5d1
strike1510 <151172451+strike1510@users.noreply.github.com>
2024-10-24 13:19:35 +0200

    maj

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "timer.h"
+
+int main() {
+
+    while(0 < 10){
+        startTimer();
+        displayTime();
+    }
+    stopTimer();
+
+
+    return 0;
+}
