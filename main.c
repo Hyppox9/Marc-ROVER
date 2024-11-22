0cc160e68dddcb9870219ff037f4413d4ba81f4e
strike1510 <151172451+strike1510@users.noreply.github.com>
2024-11-22 21:25:08 +0100

    test

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "map.h"
+#include "moves.h"
 
 int main() {
     t_map map;
@@ -32,5 +33,6 @@ int main() {
         printf("\n");
     }
     displayMap(map);
+
     return 0;
 }
